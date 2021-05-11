497d79044fb531cf6e6c70c9e675dfa528d8cfd7
govardanan-sl <govardanan.sl@rently.com>
2021-05-11 12:46:05 +0530

    logger added

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,8 @@
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
+import logger from "redux-logger";
 import reducer from "./reducer";
 
-const store = createStore(reducer,composeWithDevTools());
+const store = createStore(reducer,composeWithDevTools(applyMiddleware(logger)));
 
 export {store};
