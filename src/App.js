0c297c5b1835920a72b721ce75bda318c21399ef
govardanan-sl <govardanan.sl@rently.com>
2021-09-20 15:44:56 +0530

    add source map explorer

--- src/App.js
+++ src/App.js
@@ -9,57 +9,52 @@ import Register from './component/Login/Register';
 import HomeNew from './component/Home/Home';
 import ErrorBoundary from './component/ErrorBoundary/ErrorBoundary';
 import NewProfile from './component/Profile/Profile';
-import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { persistor, store } from './store/store';
 import { PersistGate } from 'redux-persist/integration/react';
-import { lazy, Suspense } from 'react';
 
 
 function App() {
   return (
-   <Router>
+    <Router>
       <div className="App">
         <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <Navbar/>
-          <div className="content">
-          <ErrorBoundary>
-            <Switch>
-              <Route exact path="/">
-                <Suspense fallback={<div>Loading...</div>}>
-                  <HomeNew/>
-                </Suspense>
-              </Route>
-              <Route exact path="/react-blog-test-v2">
-                <Suspense fallback={<div>Loading...</div>}>
-                  <HomeNew/>
-                </Suspense>
-              </Route>
-              <Route path="/login">
-                <Login/>
-              </Route>
-              <Route path="/register">
-                <Register></Register>
-              </Route>
-              <Route exact path="/profile">
-                <NewProfile/>
-              </Route>
-              <Route exact path="/profile/:id">
-                <ProfileDetails></ProfileDetails>
-              </Route>
-              <Route path="/posts/:id">
-                <PostDetails></PostDetails>
-              </Route>
-              <Route path="/old-home">
-                <Home/>
-              </Route>
-              <Route path="*">
-                <NotFound></NotFound>
-              </Route>
-            </Switch>
-          </ErrorBoundary>
-          </div>
+          <PersistGate loading={null} persistor={persistor}>
+            <Navbar />
+            <div className="content">
+              <ErrorBoundary>
+                <Switch>
+                  <Route exact path="/">
+                    <HomeNew />
+                  </Route>
+                  <Route exact path="/react-blog-test-v2">
+                    <HomeNew />
+                  </Route>
+                  <Route path="/login">
+                    <Login />
+                  </Route>
+                  <Route path="/register">
+                    <Register></Register>
+                  </Route>
+                  <Route exact path="/profile">
+                    <NewProfile />
+                  </Route>
+                  <Route exact path="/profile/:id">
+                    <ProfileDetails></ProfileDetails>
+                  </Route>
+                  <Route path="/posts/:id">
+                    <PostDetails></PostDetails>
+                  </Route>
+                  <Route path="/old-home">
+                    <Home />
+                  </Route>
+                  <Route path="*">
+                    <NotFound></NotFound>
+                  </Route>
+                </Switch>
+              </ErrorBoundary>
+            </div>
           </PersistGate>
         </Provider>
       </div>
